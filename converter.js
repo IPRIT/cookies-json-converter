801332102d80edf28c15d820712636829616b711
Alexander Belov <asb@rdw.ru>
2018-04-26 13:56:53 +0300

    Initial commit

---
+++ converter.js
@@ -0,0 +1,67 @@
+// https://regex101.com/r/4f1Nx6/1/
+var COOKIE_REGEXP = /^(\.?([a-zA-Zа-яА-ЯёЁ0-9-_]+\.?)*)\s(true|false)\s(\/([a-zA-Zа-яА-ЯёЁ0-9-_.]+\/?)*)\s(true|false)\s(\d+)\s(.*)\t(.*)/gmiu;
+var DEFAULT_OPTIONS = {
+    ignoreErrors: false,
+    domainRegExp: /.*/
+};
+
+/**
+ * @param {object} options target
+ * @param {object} defaultOptions
+ */
+function mergeOptions (options, defaultOptions) {
+    return Object.assign({}, defaultOptions, options || {});
+}
+
+/**
+ * Throws errors conditionally if `ignoreErrors` is true
+ * @param {string|*} error
+ * @param {boolean} ignore
+ */
+function throwError (error, ignore) {
+    if (!ignore) {
+        throw (typeof error === 'string' ? new Error(error) : error);
+    }
+}
+
+/**
+ * @param {string} string - source text
+ * @param {object} options
+ */
+function convert (string, options) {
+    options = mergeOptions(options, DEFAULT_OPTIONS);
+
+    var domainRegExp = options.domainRegExp;
+    var ignoreErrors = options.ignoreErrors;
+
+    if (!string) {
+        throwError('Source string not specified', ignoreErrors);
+    }
+
+    var match, list = [];
+    while ((match = COOKIE_REGEXP.exec(string)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (match.index === COOKIE_REGEXP.lastIndex) {
+            COOKIE_REGEXP.lastIndex++;
+        }
+        var isSecure = match[6] === 'TRUE';
+        var expiry = Number(match[7]);
+
+        list.push({
+            domain: match[1],
+            expires: new Date(expiry * 1000).toUTCString(),
+            expiry: expiry,
+            httponly: isSecure,
+            name: match[8],
+            path: match[4],
+            secure: isSecure,
+            value: match[9]
+        });
+    }
+
+    return list.filter(function (cookieObject) {
+        return domainRegExp.test( cookieObject.domain );
+    });
+}
+
+module.exports = convert;
---
+++ test/index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var path = require('path');
+var convert = require('../index');
+
+var sourceFile = fs.readFileSync(path.join(__dirname, './source.txt')).toString();
+
+/**
+ * @param {string|number} postfix
+ * @param {object} debugObject
+ */
+function writeDebugFile (postfix, debugObject) {
+    postfix = postfix || '';
+    var debugFilePath = path.join(__dirname, './results/result' + postfix + '.json');
+    try {
+        // ensure we haven't any files with specified name
+        fs.unlinkSync( debugFilePath );
+    } catch (e) {}
+    fs.writeFileSync( debugFilePath, JSON.stringify(debugObject, null, 2) );
+}
+
+var testDomains = [
+    /^(\.hh\.ru)/,
+    /nanosemantics\.ru/,
+    /\.vb\.yandex\.addons/
+];
+
+for (var domainIndex in testDomains) {
+    writeDebugFile(
+        domainIndex,
+        convert(sourceFile, {
+            domainRegExp: testDomains[ domainIndex ],
+            ingoreErrors: true
+        })
+    );
+}
